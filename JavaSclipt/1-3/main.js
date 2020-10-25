d50670bb0eda5453bcae79deeed2cdca7f8475c5
oriaki1231 <60604379+oriaki1231@users.noreply.github.com>
2020-10-25 20:32:10 +0900

    1-3

---
+++ JavaSclipt/1-3/main.js
@@ -0,0 +1,33 @@
+// 問1
+let a = 11;
+let b = 12;
+
+if ( a != 10 ) {
+    console.log(a);
+}
+
+if ( 10 < b < 20 && b % 2 === 0 ) {
+    console.log(b);
+}
+
+// 問２
+let x = 10;
+
+if ( x >= 10 && x <=20 ) {
+    console.log("成功です！");
+}
+
+else {
+    console.log("失敗です。。。")
+}
+
+// 問３
+let y = 0;
+
+if (y % 2 === 0) {
+    console.log("偶数です");
+}
+//奇数の条件式
+if (y % 2 !== 0) {
+    console.log("奇数です");
+}
