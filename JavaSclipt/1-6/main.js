50abeb090702c39cbe386aa656362c216006295a
oriaki1231 <60604379+oriaki1231@users.noreply.github.com>
2020-10-31 15:51:42 +0900

    1-6

---
+++ JavaSclipt/1-6/main.js
@@ -0,0 +1,16 @@
+// 問１
+
+let num = [10,15,20,25];
+
+console.log(num[0] + "は偶数です。");
+console.log(num[2] + "は偶数です。");
+
+// 問２
+
+let car = {
+    gass : 20.5,
+    number : 1234,
+};
+
+console.log("ガソリンは" + car.gass + "です。");
+console.log("ナンバーは" + car.number + "です。");
